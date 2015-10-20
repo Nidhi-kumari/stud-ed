112a71a8aa1ffc78f82ae2d32cf49784da310f54
jharvard <nidhi81194@gmail.com>
2015-10-20 09:59:01 -0400

    initial commit

---
+++ stued/src/solution/Database.java
@@ -0,0 +1,8 @@
+package solution;
+
+public interface Database {
+String DRIVER="oracle.jdbc.driver.OracleDriver";
+String CON_STRING="jdbc:oracle:thin:@localhost:1521:xe";
+String USERNAME="system";
+String PASSWORD="oracle";
+}
